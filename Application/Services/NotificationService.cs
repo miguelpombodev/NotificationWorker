f0844d68e2dce67432a691ed2f0a1e934b0777df
Miguel Pombo <mp96dev@gmail.com>
2026-03-26 18:32:45 -0300

    feat: add log when called handler does not exists

--- Application/Services/NotificationService.cs
+++ Application/Services/NotificationService.cs
@@ -3,13 +3,23 @@ using NotificationWorker.Domain.Models;
 
 namespace NotificationWorker.Application.Services;
 
-public class NotificationService(IEnumerable<INotificationHandler> handlers) : INotificationService
+public class NotificationService(IEnumerable<INotificationHandler> handlers, ILogger<NotificationService> logger)
+    : INotificationService
 {
     public async Task ProcessAsync(NotificationRequested notification)
     {
         var handler = handlers.FirstOrDefault(n => n.Channel == notification.Channel);
 
-        if (handler is null) throw new InvalidOperationException("Handler does not exist");
+        if (handler is null)
+        {
+            logger.LogInformation(
+                "[ERROR] There was an failed attempt to reach an unkwown channel. Channel Option: {ChannelOption}, Correlation Id: {CorrelationId}, Exception Type: {ExceptionType}",
+                notification.Channel,
+                notification.Id,
+                nameof(InvalidOperationException));
+            
+            throw new InvalidOperationException("Handler does not exist");
+        }
 
         await handler.HandleAsync(notification);
     }
