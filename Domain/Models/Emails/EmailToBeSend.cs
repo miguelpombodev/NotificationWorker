1f4239c9a6b8faca41b526ad222faa0a47b252b4
Miguel Pombo <mp96dev@gmail.com>
2026-03-26 16:03:18 -0300

    feat: create ParseToBytes method in EmailToBeSend.cs file

--- Domain/Models/Emails/EmailToBeSend.cs
+++ Domain/Models/Emails/EmailToBeSend.cs
@@ -1,10 +1,21 @@
+using System.Text;
+using Newtonsoft.Json;
 using NotificationWorker.Domain.Models.Base;
 
 namespace NotificationWorker.Domain.Models.Emails;
 
 public class EmailToBeSend : Message
 {
+    
     public string To { get; set; }
     public string Subject { get; set; }
     public string Body { get; set; }
+
+    public byte[] ParseToBytes()
+    {
+        var json = JsonConvert.SerializeObject(this);
+        var bytes = Encoding.UTF8.GetBytes(json);
+
+        return bytes;
+    }
 }
