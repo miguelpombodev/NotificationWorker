efb8a051ea0407c374afe0c610c9d31d458ef57b
Miguel Pombo <mp96dev@gmail.com>
2026-03-26 16:03:54 -0300

    refactor: rearrange some code smells in EmailDispatcher.cs

--- Application/Services/EmailDispatcher.cs
+++ Application/Services/EmailDispatcher.cs
@@ -5,40 +5,45 @@ using Newtonsoft.Json;
 using NotificationWorker.Application.Contracts;
 using NotificationWorker.Domain.Models;
 using NotificationWorker.Domain.Models.Emails;
+using NotificationWorker.Domain.Models.Providers;
 using NotificationWorker.Infrastructure;
 using NotificationWorker.Infrastructure.Templates.TemplatesModels;
 using RabbitMQ.Client;
 
 namespace NotificationWorker.Application.Services;
 
-public class EmailDispatcher(ITemplateRenderer templateRenderer, ILogger<EmailDispatcher> logger)
+public class EmailDispatcher(
+    ITemplateRenderer templateRenderer,
+    ILogger<EmailDispatcher> logger,
+    IOptions<RabbitMqOptions> rabbitMqOptions)
     : IEmailDispatcher
 {
-    private readonly string _emailSenderExchange = "sub-email-sender-exchange";
-    private readonly string _emailSenderRk = "sub-email";
+    private readonly RabbitMqOptions _rabbitOptions = rabbitMqOptions.Value;
 
     public async Task SendAsync(NotificationRequested notification)
     {
         await RetryPolicies.EmailRetry.ExecuteAsync(async () =>
         {
             var model = MapTemplate(notification);
-            var body = await templateRenderer.RenderAsync(notification.Template, model);
-            
+            var body = await templateRenderer.RenderAsync(notification.Template, model.TemplateModel);
+
             await SendEmail(notification.Recipient, body);
         });
     }
 
-    private object MapTemplate(NotificationRequested notification)
+    private EmailPayload MapTemplate(NotificationRequested notification)
     {
         return notification.Template switch
         {
-            "welcome" => new WelcomeTemplateModel
-            {
-                Name = notification.Data["name"]?.ToString() ?? string.Empty,
-                Email = notification.Recipient,
-                LoginUrl = notification.Data["loginUrl"]?.ToString() ?? string.Empty
-            },
-
+            "welcome" => new EmailPayload(
+                Subject: notification.Data["subject"].ToString() ?? string.Empty,
+                TemplateName: notification.Template,
+                TemplateModel: new WelcomeTemplateModel
+                {
+                    Name = notification.Data["name"]?.ToString() ?? string.Empty,
+                    Email = notification.Recipient,
+                    LoginUrl = notification.Data["loginUrl"]?.ToString() ?? string.Empty
+                }),
             _ => throw new InvalidOperationException("Invalid template")
         };
     }
@@ -46,41 +51,45 @@ public class EmailDispatcher(ITemplateRenderer templateRenderer, ILogger<EmailDi
     private async Task SendEmail(string recipient, string body)
     {
         logger.LogInformation(
-            "Creating connection and channel with Email Worker Queue. Exchange: {EmailSenderExchange}, Routing Key: {}",
-            _emailSenderExchange,
-            _emailSenderRk
+            "Creating connection and channel with Email Worker Queue. Exchange: {EmailSenderExchange}, Routing Key: {EmailSenderRoutingKey}",
+            _rabbitOptions.EmailSenderQueueExchangeName,
+            _rabbitOptions.EmailSenderQueueRoutingKeyName
         );
 
         var factory = new ConnectionFactory
         {
-            HostName = "localhost",
-            Port = 5672,
-            UserName = "admin",
-            Password = "admin123"
+            HostName = _rabbitOptions.EmailSenderHostName,
+            Port = _rabbitOptions.EmailSenderQueuePort,
+            UserName = _rabbitOptions.EmailSenderQueueUserName,
+            Password = _rabbitOptions.EmailSenderQueuePassword
         };
 
         await using var connection = await factory.CreateConnectionAsync();
         await using var channel = await connection.CreateChannelAsync();
 
-        var message = new EmailToBeSend
+        var message = BuildMessage(recipient, body);
+
+        logger.LogInformation("Created Email Model with subject {MessageSubject} by ID: {MessageId}",
+            message.Subject,
+            message.Id
+        );
+
+        await channel.BasicPublishAsync(
+            exchange: _rabbitOptions.EmailSenderQueueExchangeName,
+            routingKey: _rabbitOptions.EmailSenderQueueRoutingKeyName,
+            body: message.ParseToBytes()
+        );
+
+        logger.LogInformation("Email message sent successfully! MessageId: {MessageId}", message.Id);
+    }
+
+    private EmailToBeSend BuildMessage(string recipient, string body)
+    {
+        return new EmailToBeSend
         {
             To = recipient,
             Subject = "Welcome",
             Body = body
         };
-
-        logger.LogInformation("Created Email Model with subject {MessageSubject} by ID: {MessageId}", message.Subject,
-            message.Id);
-
-        var json = JsonConvert.SerializeObject(message);
-        var bodyBytes = Encoding.UTF8.GetBytes(json);
-
-        await channel.BasicPublishAsync(
-            exchange: _emailSenderExchange,
-            routingKey: _emailSenderRk,
-            body: bodyBytes
-        );
-
-        logger.LogInformation("Email message sent successfully! MessageId: {MessageId}", message.Id);
     }
 }
