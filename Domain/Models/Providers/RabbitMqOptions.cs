0141df1d603e6eaf319aef16c837f640555531cd
Miguel Pombo <mp96dev@gmail.com>
2026-03-26 16:02:51 -0300

    fix: add EmailSender Rabbit MQ options in RabbitMqOptions.cs

--- Domain/Models/Providers/RabbitMqOptions.cs
+++ Domain/Models/Providers/RabbitMqOptions.cs
@@ -1,4 +1,4 @@
-namespace NotificationWorker.Domain.Models;
+namespace NotificationWorker.Domain.Models.Providers;
 
 public class RabbitMqOptions
 {
@@ -9,5 +9,12 @@ public class RabbitMqOptions
     public string QueueName { get; set; } = "notification-worker";
     public string ExchangeName { get; set; } = "notification-worker-exchange";
     public string RoutingKeyName { get; set; } = "notification-worker-rk";
+    public string EmailSenderHostName { get; set; } = "localhost";
+    public string EmailSenderQueueName { get; set; } = "localhost";
+    public int EmailSenderQueuePort { get; set; } = 5672;
+    public string EmailSenderQueueUserName { get; set; } = "admin";
+    public string EmailSenderQueuePassword { get; set; } = "admin123";
+    public string EmailSenderQueueExchangeName { get; set; } = "sub-email-sender-exchange";
+    public string EmailSenderQueueRoutingKeyName { get; set; } = "sub-email";
     public ushort PrefetchCount { get; set; } = 10;
 }
